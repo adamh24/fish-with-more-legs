31ff624eda348de8f9710411dd6db33e94903307
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2026-03-11 21:02:24 -0700

    scroller

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,10 +2,35 @@ import Hero from '../Components/Hero';
 import '../Style/Home.css';
 
 function Home() {
+  const classicCocktails = [
+    'Negroni',
+    'Old Fashioned',
+    'Daiquiri',
+    'Martini',
+    'Manhattan',
+    'Margarita',
+    'Whiskey Sour',
+    'Mojito',
+    'Sidecar',
+    'Boulevardier',
+    'Tom Collins',
+    'Cosmopolitan'
+  ];
+
   return (
     <div className="home-container">
         
         <Hero />
+
+        <div className="cocktail-banner" aria-hidden="true">
+          <div className="cocktail-banner-track">
+            {[...classicCocktails, ...classicCocktails].map((name, index) => (
+              <span className="cocktail-banner-item" key={`${name}-${index}`}>
+                {name}
+              </span>
+            ))}
+          </div>
+        </div>
 
     </div>
   )
