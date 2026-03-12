4382db614a0fa2a9faae5f229aef41db8ea1f2a0
adamha24 <adam.hannay@yahoo.co.uk>
2026-03-12 04:03:37 +0000

    Contact FT

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,12 @@
 import Hero from '../Components/Hero';
 import '../Style/Home.css';
 
+
 function Home() {
   return (
     <div className="home-container">
         
         <Hero />
-
-        <FastTravel />
 
     </div>
   )
