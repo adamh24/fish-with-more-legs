408cceb49ec3e4eec7d7cb4ee00c16d430bbd6ef
adamha24 <adam.hannay@yahoo.co.uk>
2026-03-12 03:12:26 +0000

    fast travel

---
+++ src/Components/FastTravel.js
@@ -0,0 +1,13 @@
+import '...FastTravel.css'
+
+function FastTravel() {
+
+    return (
+        <div className="fast-travel-container">
+            
+        </div>
+
+    )
+}
+
+export default FastTravel;
